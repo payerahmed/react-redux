136ad52bb3b0d964ef87faf0d9a89806e826cd05
payerahmed <mdpayer125@gmail.com>
2021-07-13 23:33:34 +0600

    multiple reducers and combine reducers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 const redux = require('redux');
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers
 
 function App() {
   return <div className='App'></div>;
 }
 
 const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM';
 
 function buycake() {
   return {
@@ -14,11 +16,24 @@ function buycake() {
     info: ' first redux action'
   };
 }
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM
+  };
+}
 
-const initialState = {
+// const initialState = {
+//   numberOfCake: 10,
+//   buyIceCream: 15
+// };
+const initialCakeState = {
   numberOfCake: 10
 };
-const reducer = (state = initialState, action) => {
+
+const initialIceCream = {
+  numberOfIceCream: 15
+};
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
@@ -30,7 +45,43 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-const store = createStore(reducer);
+const iceCreamReducer = (state = initialIceCream, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        numberOfIceCream: state.numberOfIceCream - 1
+      };
+
+    default:
+      return state;
+  }
+};
+
+
+
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case BUY_CAKE:
+//       return {
+//         ...state,
+//         numberOfCake: state.numberOfCake - 1
+//       };
+
+//     case BUY_ICECREAM:
+//       return {
+//         ...state,
+//         buyIceCream: state.buyIceCream - 1
+//       };
+//     default:
+//       return state;
+//   }
+// };
+const rootReducer = combineReducers({
+  cake:cakeReducer,
+  iceCream:iceCreamReducer
+})
+const store = createStore( rootReducer);
 console.log('initial state', store.getState());
 const unsubscribe = store.subscribe(() => {
   console.log('update state', store.getState());
@@ -38,6 +89,8 @@ const unsubscribe = store.subscribe(() => {
 store.dispatch(buycake());
 store.dispatch(buycake());
 store.dispatch(buycake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 unsubscribe();
 
-export default App;
+ export default App;
