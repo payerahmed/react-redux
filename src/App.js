f854a3a43c0f09ec60646dbd9bf2905537459202
payerahmed <mdpayer125@gmail.com>
2021-07-13 20:02:10 +0600

    redux basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+const redux = require('redux');
+const createStore = redux.createStore;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className='App'></div>;
 }
+
+const BUY_CAKE = 'BUY_CAKE';
+
+function buycake() {
+  return {
+    type: BUY_CAKE,
+    info: ' first redux action'
+  };
+}
+
+const initialState = {
+  numberOfCake: 10
+};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numberOfCake: state.numberOfCake - 1
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+console.log('initial state', store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log('update state', store.getState());
+});
+store.dispatch(buycake());
+store.dispatch(buycake());
+store.dispatch(buycake());
+unsubscribe();
 
 export default App;
